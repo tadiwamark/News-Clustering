e10657f22fb7c639af038f1d58e27b01b24f51c9
Tadiwanashe Nyaruwata <64325379+tadiwamark@users.noreply.github.com>
2024-05-09 14:20:07 +0200

    Update app.py

--- app.py
+++ app.py
@@ -29,6 +29,34 @@ def cluster_stories(tfidf_matrix, num_clusters=4):
     km.fit(tfidf_matrix)
     return km.labels_
 
+
+def find_optimal_clusters(data, max_k=10):
+    iters = range(2, max_k+1)
+    sse = []
+    silhouette_scores = []
+    
+    for k in iters:
+        model = KMeans(n_clusters=k, random_state=42)
+        model.fit(data)
+        sse.append(model.inertia_)
+        labels = model.labels_
+        silhouette_scores.append(silhouette_score(data, labels))
+        
+    f, ax = plt.subplots(1, 2, figsize=(15, 5))
+    ax[0].plot(iters, sse, marker='o')
+    ax[0].set_xlabel('Cluster Count')
+    ax[0].set_ylabel('SSE')
+    ax[0].set_title('Elbow Method')
+    
+    ax[1].plot(iters, silhouette_scores, marker='o')
+    ax[1].set_xlabel('Cluster Count')
+    ax[1].set_ylabel('Silhouette Score')
+    ax[1].set_title('Silhouette Scores')
+    
+    plt.show()
+    return sse, silhouette_scores
+
+
 # Main app function
 def main():
     st.title('News Story Clustering')
@@ -40,6 +68,9 @@ def main():
 
     # Preprocess and cluster data
     tfidf_matrix = preprocess_data(data)
+    if st.button('Analyze Clustering'):
+        find_optimal_clusters(tfidf_matrix)
+
     data['cluster'] = cluster_stories(tfidf_matrix)
 
     # User input for selecting a cluster
