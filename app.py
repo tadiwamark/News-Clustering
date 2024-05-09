fe8eaebfceb7bb33f378d3ef9d21bcf173b3f170
Tadiwanashe Nyaruwata <64325379+tadiwamark@users.noreply.github.com>
2024-05-09 14:27:54 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import numpy as np
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.cluster import KMeans, DBSCAN, AgglomerativeClustering
 import matplotlib.pyplot as plt
